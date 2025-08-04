23e5278ea509e21d1038919f47b7b20f8a2fc04d
angwx <angwx90@gmail.com>
2025-08-04 15:05:42 +0800

    updated add command for verification
    added custom exception class

--- src/commands/AddCommand.java
+++ src/commands/AddCommand.java
@@ -2,6 +2,8 @@ package commands;
 
 import data.Receiver;
 
+import java.util.Stack;
+
 public class AddCommand implements Command {
     private final Receiver receiver;
     private final String payload;
@@ -10,6 +12,33 @@ public class AddCommand implements Command {
     public AddCommand(Receiver receiver, String payload){
         this.receiver = receiver;
         this.payload = payload;
+    }
+
+    // Validation for ADD command
+    public void validateAndExecute(String[] payloadArr) {
+        if (payloadArr.length != 4) {
+            System.out.println("Add command format: add <firstName> <lastName> <email>\n" +
+                    "Example: add John Doe john@example.com");
+            return;
+        }
+
+    // Layer 1: Basic parameter checks
+    String firstName = payloadArr[1];
+    String lastName = payloadArr[2];
+    String email = payloadArr[3];
+
+    if (firstName.isEmpty()){
+        System.out.println("First name is empty");
+        return;
+    }
+    if (lastName.isEmpty()){
+        System.out.println("Last name is empty");
+    }
+    if (email.isEmpty()){
+        System.out.println("Email is empty");
+    }
+
+
     }
 
     @Override
