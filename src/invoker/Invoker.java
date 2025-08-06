fde2e8eddffefc837d425563e6b116f4c2d12d4c
angwx <angwx90@gmail.com>
2025-08-06 15:23:38 +0800

    Removed throw CustomException in Driver main() and Receiver toStoreFile()

--- src/invoker/Invoker.java
+++ src/invoker/Invoker.java
@@ -3,6 +3,10 @@ package invoker;
 import commands.Command;
 import java.util.Stack;
 
+/**
+ * Invoker class to initiate and trigger commands.
+ * Contains Command array to store list of commands to execute.
+ */
 public class Invoker {
     private Command[] cmdToExecute;
 
