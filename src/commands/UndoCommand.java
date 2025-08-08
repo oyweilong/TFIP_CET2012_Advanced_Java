71697d5df5f77671f7b7536516596f5afbac1138
angwx <angwx90@gmail.com>
2025-08-08 17:25:22 +0800

    Updated javadoc for UndoCommand

--- src/commands/UndoCommand.java
+++ src/commands/UndoCommand.java
@@ -2,20 +2,35 @@ package commands;
 
 import data.Receiver;
 import exceptions.CustomException;
-
 import java.util.EmptyStackException;
 import java.util.Stack;
 
+/**
+ * Concrete Command Class to implement Undo Command
+ */
 public class UndoCommand implements Command {
+    // ===== FIELDS =====
     private final Receiver receiver;
     private Stack<Command> history;
     public boolean isUndoable = false;
 
+    // ===== CONSTRUCTORS =====
+
+    /**
+     * Constructor for Undo Command
+     * @param receiver receiver instance to accept commands
+     * @param history input containing data items
+     */
     public UndoCommand(Receiver receiver, Stack<Command> history){
        this.receiver = receiver;
        this.history = history;
     }
 
+    /**
+     * Execution method for Undo Command.
+     * @return true if there is command to undo.
+     * @throws EmptyStackException if there is no command to undo.
+     */
     @Override
     public boolean execute() throws EmptyStackException{
         try{
@@ -31,6 +46,11 @@ public class UndoCommand implements Command {
             throw new CustomException("No commands to undo");
         }
     }
+
+    /**
+     * Getter method for an undoable flag for Undo Command
+     * @return isUndoable field
+     */
     @Override
     public boolean checkUndoable(){
         return isUndoable;
